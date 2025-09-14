c98efdbe0949e7b59e7f396cb7cde23733b072d3
Afolabi Ayomide Emmanuel <138625749+Spagero763@users.noreply.github.com>
2025-09-14 15:00:46 +0100

    Update Badge_flattened.sol

--- Badge_flattened.sol
+++ Badge_flattened.sol
@@ -11,7 +11,7 @@ pragma solidity >=0.4.16;
  
 interface IERC165 {
     /**
-     * @dev Returns true if this contract implements the interface defined by
+     * @dev  true if this contract implements the interface defined by
      * `interfaceId`. See the corresponding
      * https://eips.ethereum.org/EIPS/eip-165#how-interfaces-are-identified[ERC section]
      * to learn more about how these ids are created.
