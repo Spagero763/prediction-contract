3b8ae51fc8ebfff754a513d2cd3ef3da29458cc4
Afolabi Ayomide Emmanuel <138625749+Spagero763@users.noreply.github.com>
2025-09-14 15:32:24 +0100

    Update Badge_flattened.sol

--- Badge_flattened.sol
+++ Badge_flattened.sol
@@ -28,11 +28,7 @@ interface IERC721 is IERC165 {
 
     /**
      * @dev Emitted when `owner` enables `approved` to manage the `tokenId` token.
-     */
-    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
-
-    /**
-     * @dev Returns the number of tokens in ``owner``'s account.
+'s account.
      */
     function balanceOf(address owner) external view returns (uint256 balance);
 
