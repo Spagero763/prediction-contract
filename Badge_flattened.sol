b61fc45461f85da91932fc2f3f055f3df03e1c14
Afolabi Ayomide Emmanuel <138625749+Spagero763@users.noreply.github.com>
2025-09-14 15:32:44 +0100

    Update Badge_flattened.sol

--- Badge_flattened.sol
+++ Badge_flattened.sol
@@ -61,12 +61,7 @@ interface IERC721 is IERC165 {
      * Requirements:
      *
      * - `from` cannot be the zero address.
-     * - `to` cannot be the zero address.
-     * - `tokenId` token must exist and be owned by `from`.
-     * - If the caller is not `from`, it must have been allowed to move this token by either {approve} or
-     *   {setApprovalForAll}.
-     * - If `to` refers to a smart contract, it must implement {IERC721Receiver-onERC721Received}, which is called upon
-     *   a safe transfer.
+     * - `to` cannot be the zero address
      *
      * Emits a {Transfer} event.
      */
