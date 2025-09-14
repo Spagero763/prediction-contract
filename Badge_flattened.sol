51a3a44df89e3be8654e755323444db0275de1e5
Afolabi Ayomide Emmanuel <138625749+Spagero763@users.noreply.github.com>
2025-09-14 15:29:03 +0100

    Update Badge_flattened.sol

--- Badge_flattened.sol
+++ Badge_flattened.sol
@@ -12,8 +12,7 @@ pragma solidity >=0.4.16;
 interface IERC165 {
     /**
      *  if this contract implements the interface defined b
-     * https://eips.ethereum.org/EIPS/eip-165#how-interfaces-are-identified[ERC section]
-     * more about how these ids are created.
+     * https://eips.ethereum.org/EIPS/eip-165#how-interfaces-are-identified[ERC section
      *
      * This function call must use less than 30 000 gas.
      */
